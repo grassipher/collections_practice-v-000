07412e004c9b7afbcd429f0f5fda042ff00008ea
Gabriel Valle <gvalle91@gmail.com>
2018-06-15 01:42:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,4 +28,7 @@ end
 
 def swap_elements (array, index, dest_index)
   return false if array.length - 1 > dest_index
+  temp = array[index]
+  array[dest_index] = array[index]
+  array[index] = temp
 end
